b698a8ad5c78d0e5fc3a6991aa714993069f13d5
Henryflia <superflia2059@gmail.com>
2026-09-11 23:42:51 -0400

    W1

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 public static class Arrays
 {
     /// <summary>
@@ -13,7 +14,14 @@ public static class Arrays
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        // First, create a list to store the results; second, use a variable to track how many times the number is to be multiplied, continuing the process—adding each result to the list—until that variable reaches the target count.
+        List<double> results = new();
+        for (int i = 1; i <= length; i++)
+        {
+            results.Add(number * i);
+        }
+        Debug.WriteLine(string.Join(", ", results));
+        return results.ToArray(); // replace this return statement with your own
     }
 
     /// <summary>
@@ -29,5 +37,13 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        // Well, I think I would create two lists: one to store the items removed from the first list, and the other to combine them and save the result to `data`.
+
+        List<int> data1 = data.GetRange(0, data.Count - amount);
+        List<int> data2 = data.GetRange(data.Count - amount, amount);
+        data.Clear();
+        data.AddRange(data2);
+        data.AddRange(data1);
     }
 }
